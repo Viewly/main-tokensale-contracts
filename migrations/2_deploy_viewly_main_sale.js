daa6263fe180064b9f90623388a3648a15807f6f
Uros Jurglic <jurglic@gmail.com>
2018-02-20 02:35:43 +0100

    Set testnet and mainnet beneficiary addresses
    Mainnet uses standard trezor controlled beneficiary address.

--- migrations/2_deploy_viewly_main_sale.js
+++ migrations/2_deploy_viewly_main_sale.js
@@ -4,8 +4,8 @@ module.exports = function(deployer, network) {
   let beneficiary = {
     "test":    "0xb81546A2E9e5C392B07e6f56cED4d43ffbaaAB56",
     "ganache": "0xb81546A2E9e5C392B07e6f56cED4d43ffbaaAB56",
-    "kovan":   "0xb81546A2E9e5C392B07e6f56cED4d43ffbaaAB56",
-    "mainnet": "0xf03f8d65bafa598611c3495124093c56e8f638f0",
+    "kovan":   "0x00B17A6e5eDb9C4d9D01493A360f0B9a95EeDE3e",
+    "mainnet": "0x80e36493e20926f8201e7C7dB2e20d24d7D2f7E5",
   };
   console.log(`  Using beneficiary: ${beneficiary[network]}`);
 
